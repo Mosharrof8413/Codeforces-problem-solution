253de0c024235c66920d35f8ad692a89ca0548d3
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-12-23 23:04:26 +0600

    Add files via upload

---
+++ Problems-B/B. Reverse Sort.cpp
@@ -0,0 +1,114 @@
+
+/*
+ Problem Link :
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+
+
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int MX=1e7+123;
+bitset<MX>is_prime;
+vector<ll>primes;
+void primeGen(ll n)
+{
+    ll sq=sqrt(n);
+    for(ll i=3; i<=n; i+=2)is_prime[i]=1;
+
+    for(ll i=3; i<=sq; i+=2)
+    {
+        if(is_prime[i]==1)
+        {
+            for(ll j=i*i; j<=n; j+=(i+i))
+                is_prime[j]=0;
+        }
+    }
+    primes.PB(2);
+    is_prime[2]=1;
+    for(ll i=3; i<=n; i+=2)
+    {
+        if(is_prime[i]==1)primes.PB(i);
+    }
+}
+ll NOD(ll n)
+{
+    ll ans=1;
+    for(auto u : primes)
+    {
+
+        if(u*u > n)break;
+        if(n%u==0)
+        {
+            ll cnt=0;
+            while(n%u==0)
+            {
+                n/=u;
+                cnt++;
+            }
+            cnt++;
+            ans*=cnt;
+        }
+    }
+    if(n>1) ans*=2;
+    return ans;
+}
+vector<ll>v;
+const int mx=5e6+123;
+unsigned long long phi[mx];
+int main()
+{
+    optimize();
+    ll limt=1e6+123;
+    //primeGen(limt);
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        string st;
+        cin>>st;
+        string tt=st;
+        if(is_sorted(st.begin(),st.end()))
+        {
+            cout<<0<<endl;
+            continue;
+        }
+        sort(tt.begin(),tt.end());
+        vector<ll>ans;
+        for(ll i=0;i<n;i++)
+        {
+            if(st[i]!=tt[i])ans.PB(i+1);
+        }
+        cout<<1<<endl;
+        cout<<ans.size()<<" ";
+        for(auto u : ans)cout<<u<<" ";
+        cout<<endl;
+    }
+}
+
+//Alhamdulillah..
+
+
+
+
