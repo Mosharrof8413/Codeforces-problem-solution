8f4e8564a9a482c6795724e828aa856197c4b1dc
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2022-05-03 00:33:00 +0600

    Add files via upload

---
+++ Problems-B/B. Dictionary.cpp
@@ -0,0 +1,103 @@
+
+/*
+ Problem : https://codeforces.com/problemset/problem/1674/B
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+/*
+const int MX=1e6+123;
+bitset<MX>is_prime;
+vector<ll>primes;
+void primeGen(ll n)
+{
+    ll sq=sqrt(n);
+    for(ll i=3; i<=n; i+=2)is_prime[i]=1;
+
+    for(ll i=3; i<=sq; i+=2)
+    {
+        if(is_prime[i]==1)
+        {
+            for(ll j=i*i; j<=n; j+=(i+i))
+                is_prime[j]=0;
+        }
+    }
+    primes.PB(2);
+    is_prime[2]=1;
+    for(ll i=3; i<=n; i+=2)
+    {
+        if(is_prime[i]==1)primes.PB(i);
+    }
+}
+const int mod=1e9+7;
+
+*/
+
+int main()
+{
+    optimize();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+
+       string st;
+       cin>>st;
+       ll f=st[0]-'a'+1;
+       ll s=st[1]-'a'+1;
+       ll ff=25*(f-1);
+       ff+=s;
+       if(f<=s)ff--;
+       cout<<ff<<endl;
+    }
+
+}
+
+//Alhamdulillah..
+
+
+/*
+input :
+7
+ab
+ac
+az
+ba
+bc
+zx
+zy
+
+
+Output:
+
+1
+2
+25
+26
+27
+649
+650
+
+
+
+*/
+
+
+
+
+
+
+
+
