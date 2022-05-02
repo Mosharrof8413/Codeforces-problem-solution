b9b05c0f66c0ab56b4b8fe57a6662c9fad7cc4ca
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2022-05-03 00:35:05 +0600

    Add files via upload

---
+++ Problems-A/A. Number Transformation.cpp
@@ -0,0 +1,96 @@
+
+/*
+ Problem : https://codeforces.com/problemset/problem/1674/A
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+/*
+const int MX=1e6+123;
+bitset<MX>is_prime;
+vector<ll>primes;
+void primeGen(ll n)
+{
+    ll sq=sqrt(n);
+    for(ll i=3; i<=n; i+=2)is_prime[i]=1;
+
+    for(ll i=3; i<=sq; i+=2)
+    {
+        if(is_prime[i]==1)
+        {
+            for(ll j=i*i; j<=n; j+=(i+i))
+                is_prime[j]=0;
+        }
+    }
+    primes.PB(2);
+    is_prime[2]=1;
+    for(ll i=3; i<=n; i+=2)
+    {
+        if(is_prime[i]==1)primes.PB(i);
+    }
+}
+
+*/
+
+int main()
+{
+    optimize();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        if(b%a==0)
+        {
+            ll d=b/a;
+            cout<<1<<" "<<d<<endl;
+        }
+        else cout<<0<<" "<<0<<endl;
+
+    }
+
+}
+
+//Alhamdulillah..
+
+
+/*
+input :
+
+3
+3 75
+100 100
+42 13
+
+
+Output:
+
+2 5
+3 1
+0 0
+
+
+
+
+*/
+
+
+
+
+
+
+
+
