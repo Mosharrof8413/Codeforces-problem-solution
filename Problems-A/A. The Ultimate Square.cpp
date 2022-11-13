0bab7c3495709a30b188dd2e01e2aaa3fbbfc03e
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2022-11-13 17:01:57 +0600

    Add files via upload

---
+++ Problems-A/A. The Ultimate Square.cpp
@@ -0,0 +1,58 @@
+/*
+
+ Problem link : https://codeforces.com/contest/1748/problem/A
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+const int mx=20;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        n=n/2+(n%2);
+        cout<<n<<endl;
+    }
+}
+
+//Alhamdulillah..
+
+/*
+intput :
+
+3
+2
+5
+197654321
+output :
+1
+3
+98827161
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
